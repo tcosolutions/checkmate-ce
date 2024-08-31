757502b8c570323f05e814a4180127c24a1eaa0a
Marcin Kozlowski <marcinkozlowski@mbp-von-marcin.speedport.ip>
2024-08-31 20:57:59 +0200

    Update

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='checkmate3',
-    version='4.0.15',
+    version='4.0.16',
     license='MIT',
     install_requires=[
         'blitzdb3_ce',
