2a17d12ab771b09d1a651aaa1f1b62e805e6efea
Marcin Kozlowski <marcinguy@gmail.com>
2024-11-13 14:12:24 +0100

    Update

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='checkmate5',
-    version='4.0.83',
+    version='4.0.84',
     license='MIT',
     install_requires=[
         'blitzdb5',
